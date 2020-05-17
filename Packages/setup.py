8d2e0c2a1329d0e63edb142553bff1be8151e031
MikhailUskin <wurty@mail.ru>
2020-05-17 22:04:30 +0300

    upd: more correct names

--- Packages/setup.py
+++ Packages/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
  
-setup(name='SimpleObjectTracker',
+setup(name='SimpleObjectDetector',
     version='1.0.0',
     url='https://github.com/CatixBot/CatixVision',
     license='MIT',
     author='Mikhail Uskin',
     author_email='wurty@mail.ru',
-    description='Simple object tracking based on HSV color range masking',
+    description='Simple object detector based on HSV color range masking',
     packages=find_packages(),
     install_requires=['opencv-python', 'numpy'],
     zip_safe=False)
